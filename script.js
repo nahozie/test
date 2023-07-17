ee404e8707fb870847cb4512741cec710a978e4f
NahOzie <firhanfakhrurrozi19@gmail.com>
2023-07-17 21:06:05 +0700

    update 2

---
+++ script.js
@@ -0,0 +1,14 @@
+// untuk button mulai belajar
+const button = document.getElementById('btn-1');
+button.addEventListener('click', function() {
+  window.location.href = 'coba2.html'; 
+});
+// untuk button forum
+const button1 = document.getElementById('btn-0');
+button1.addEventListener('click', function() {
+    window.open("https://discord.gg/6cDcBsXJgz", "_blank");
+});
+// const main = document.getElementsByClassName('Main');
+// navigator.addEventListener('click', function(){
+//     window.location.href = 'coba.html'; 
+// });
